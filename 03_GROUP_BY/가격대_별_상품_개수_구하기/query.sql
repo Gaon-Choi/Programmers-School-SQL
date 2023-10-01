05dc71a5b9cff699b32e538ce122f5927e748a03
Gaon-Choi <choigaon1028@gmail.com>
2023-10-01 14:25:26 +0900

    20231001-3

---
+++ 03_GROUP_BY/가격대_별_상품_개수_구하기/query.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT  (T1.PRICE DIV 10000) * 10000    AS 'PRICE_GROUP'
+    ,   COUNT(*)                        AS 'PRODUCTS'
+FROM    PRODUCT T1
+GROUP BY
+        T1.PRICE DIV 10000
+ORDER BY
+        T1.PRICE DIV 10000  ASC
